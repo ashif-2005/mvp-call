00b113d3cba5cef9f25388ec14ed3a67e65b84bc
ashif-2005 <123965886+ashif-2005@users.noreply.github.com>
2024-05-16 12:49:33 +0530

    Update server.js company

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(parser.json())
 app.use(cors())
 
 const port = process.env.PORT || 8000
-const accountSid = 'AC52f0dd420b4a94411f9f989cf7749e69';
-const authToken = '5ba053a83612b047809b9498ecaff4a5';
+const accountSid = 'ACc4372b9957b7b680dde2cbc9e5338830';
+const authToken = 'c3b451ec4a9515f40f66e7ac000ddd63';
 
 app.post('/call',(req,res)=>{
     try{
@@ -40,7 +40,7 @@ app.post('/call',(req,res)=>{
             }
         }
 
-        const from = '+12513339394';
+        const from = '+14087525781';
         const to = `+91${req.body.agentNumber}`;
         const twimlUrl = `https://calltrack.onrender.com/twiml/${req.body.clientNumber}`; // URL to TwiML document that handles the call
 
